14cd0b439a0c3200d279c2fd54427ed789a262c7
GijsvdP21 <gijsvandeputte@icloud.com>
2023-02-17 13:39:48 +0100

    update

--- index.js
+++ index.js
@@ -1,33 +1,51 @@
-import express, { response } from 'express'
+// Importeer express uit de node_modules map
+import express, { json, response } from 'express'
 
-const url = 'https://whois.fdnd.nl/api/v1/squad/squad-b-2022'
-const data = await fetch(url)
-  .then((response) => response.json())
-  .catch((error) => error)
+const url = 'https://whois.fdnd.nl/api/v1/squad/'
 
-console.log(data)
-
-// Maak een nieuwe express app
+// Maak een nieuwe express app aan
 const app = express()
 
-// Stel in hoe we express gebruiken
+// Stel ejs in als template engine en geef de 'views' map door
 app.set('view engine', 'ejs')
 app.set('views', './views')
+
+// Gebruik de map 'public' voor statische resources
 app.use(express.static('public'))
 
 // Maak een route voor de index
-app.get('/', (request, response) => {
-  console.log(request.query.squad)
+app.get('/', function (req, res) {
 
-  response.render('index', data)
+  let slug = req.query.squad || 'squad-b-2022'
+  let orderBy = req.query.orderBy || 'name'
+  let squadUrl = url + slug + '?orderBy=' + orderBy + '&direction=ASC'
+
+  fetchJson(squadUrl).then((data) => {
+    data.squad.members.forEach(function(member) {
+    })
+    
+    // res.send('Hello World!')
+    res.render('index', data, slug)
+  })
+
 })
 
-// app.get('/members', (request, response) => {
-//   response.send('Joepie!!')
-// })
-
-// Stel het poortnummer in en start express
+// Stel het poortnummer in waar express op gaat luisteren
 app.set('port', process.env.PORT || 8000)
+
+// Start express op, haal het ingestelde poortnummer op
 app.listen(app.get('port'), function () {
+  // Toon een bericht in de console en geef het poortnummer door
   console.log(`Application started on http://localhost:${app.get('port')}`)
 })
+
+/**
+ * Wraps the fetch api and returns the response body parsed through json
+ * @param {*} url the api endpoint to address
+ * @returns the json response from the api endpoint
+ */
+async function fetchJson(url) {
+  return await fetch(url)
+    .then((response) => response.json())
+    .catch((error) => error)
+}
