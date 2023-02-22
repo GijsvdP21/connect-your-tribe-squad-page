1e0f850b62aab169842565fb9401777468505a33
GijsvdP21 <gijsvandeputte@icloud.com>
2023-02-22 13:30:18 +0100

    push

--- public/script/script.js
+++ public/script/script.js
@@ -18,17 +18,26 @@ personas.forEach((persoon)=>{
 })
 
 
+// Searchbar
 
+const searchbar = document.querySelector('.searchbar')
 
+searchbar.addEventListener('input', searching)
 
+function searching() {
+    let input = this.value
+    input = input.toLowerCase();
 
+    let x = document.getElementsByClassName('members');
 
-// sluit.forEach((sluiter)=>{
-// sluiter.addEventListener('click', clicker)
-// })
-
-// function clicker() {
-//     modal.classList.toggle('visible');
-//     body.classList.toggle('invisible');
-//     console.log('werkt');
-// }
+    for (i = 0; i < x.length; i++) {
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display = 'none';            
+        }
+        else {
+            x[i].style.display = 'flex';
+        }
+        console.log(x[i].hidden)
+    }
+    console.log(this.value)
+}
